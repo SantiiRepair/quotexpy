c3b4f366395964e79f03f3fddec4a4850440e37e
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-08-31 17:33:04 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,32 +1,22 @@
 """The python wrapper for IQ Option API package setup."""
-from setuptools import (setup, find_packages)
+from pathlib import Path
 
+from setuptools import find_packages, setup
+
+this_dir = Path(__file__).parent
+module_dir = this_dir / "vault_decryptor"
+
+requirements = []
+requirements_path = this_dir / "requirements.txt"
+if requirements_path.is_file():
+    with open(requirements_path, "r", encoding="utf-8") as requirements_file:
+        requirements = requirements_file.read().splitlines()
+
+# -----------------------------------------------------------------------------
 setup(
     name="pyquotex",
-    version='1.0',
+    version='1.0.0',
     packages=find_packages(),
-    install_requires=[
-        "appdirs==1.4.4",
-        "beautifulsoup4==4.11.2",
-        "certifi==2022.12.7",
-        "charset-normalizer==3.1.0",
-        "cloudscraper==1.2.69",
-        "greenlet==2.0.1",
-        "idna==3.4",
-        "importlib-metadata==6.2.0",
-        "playwright==1.32.1",
-        "pyee==9.0.4",
-        "pyparsing==3.0.9",
-        "requests==2.28.2",
-        "requests-toolbelt==0.10.1",
-        "simplejson==3.18.3",
-        "soupsieve==2.4",
-        "tqdm==4.65.0",
-        "typing_extensions==4.5.0",
-        "urllib3==1.26.14",
-        "websocket-client==1.3.3",
-        "websockets==10.4",
-        "zipp==3.15.0"
-    ],
+    install_requires=requirements,
     include_package_data=True,
 )
