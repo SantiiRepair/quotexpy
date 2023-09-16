8176de0ddb2fdfd2155389fd4fed409e1bfa3b0a
Santiago Ramirez <94815926+SantiiRepair@users.noreply.github.com>
2023-09-15 20:27:35 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ if requirements_path.is_file():
     with open(requirements_path, "r", encoding="utf-8") as requirements_file:
         requirements = requirements_file.read().splitlines()
 
+with open("README.md", "r", encoding="utf-8") as md:
+    readme = md.read()
 # -----------------------------------------------------------------------------
 
 setup(
@@ -21,9 +23,10 @@ setup(
     author="Santiago Ramirez",
     author_email="santiirepair@gmail.com",
     description="📈 QuotexPy is a library for interact with qxbroker easily.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     install_requires=requirements,
     include_package_data=True,
     python_requires=">=3.10",
-    data_files=[("", ["README.md", "LICENSE"])],
 )
