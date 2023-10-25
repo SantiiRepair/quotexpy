45ee1146a54e2aac3de80928bb6bb8a944b1d4ac
ricardospinoza <ricardospinoza@gmail.com>
2023-10-25 15:46:01 -0300

    alter atribute username to email

--- quotexpy/http/login.py
+++ quotexpy/http/login.py
@@ -10,13 +10,13 @@ class Login(Browser):
     base_url = 'qxbroker.com'
     https_base_url = f'https://{base_url}'
 
-    async def __call__(self, username, password):
+    async def __call__(self, email, password):
         """Method to get Quotex API login http request.
-        :param str username: The username of a Quotex server.
+        :param str email: The email of a Quotex server.
         :param str password: The password of a Quotex server.
         :returns: The instance of :class:`playwright.cookies`.
         """
-        self.username = username
+        self.email = email
         self.password = password
         self.ssid, self.cookies = await self.get_cookies_and_ssid()
         return self.ssid, self.cookies
